8412e1f7f48cddfb54f9f9773fbff9431739ec3b
woojtekk <wojtek.zajaczkowski@gmail.com>
2019-03-01 15:05:03 +0100

    cleaning

--- test.py
+++ test.py
@@ -1,75 +1,18 @@
-from Tkinter import *
-from random import randint
+import numpy as np
+import cv2
 
-# these two imports are important
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
-import time
-import threading
+img = cv2.imread('image2.png')
+imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(imgray,127,255,0)
+contours, hierarchy= cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+# print(cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE))
 
-continuePlotting = False
+print(contours)
 
-
-def change_state():
-    global continuePlotting
-    if continuePlotting == True:
-        continuePlotting = False
-    else:
-        continuePlotting = True
+#cv2.drawContours(img, contours, 3, (0,255,0), 3)
 
 
-def data_points():
-    f = open("data.txt", "w")
-    for i in range(10):
-        f.write(str(randint(0, 10)) + '\n')
-    f.close()
-
-    f = open("data.txt", "r")
-    data = f.readlines()
-    f.close()
-
-    l = []
-    for i in range(len(data)):
-        l.append(int(data[i].rstrip("\n")))
-    return l
+#cv2.imshow("Robust", img)
+#cv2.waitKey(0),
 
 
-def app():
-    # initialise a window.
-    root = Tk()
-    root.config(background='white')
-    root.geometry("1000x700")
-
-    lab = Label(root, text="Live Plotting", bg='white').pack()
-
-    fig = Figure()
-
-    ax = fig.add_subplot(111)
-    ax.set_xlabel("X axis")
-    ax.set_ylabel("Y axis")
-    ax.grid()
-
-    graph = FigureCanvasTkAgg(fig, master=root)
-    graph.get_tk_widget().pack(side="top", fill='both', expand=True)
-
-    def plotter():
-        while continuePlotting:
-            ax.cla()
-            ax.grid()
-            dpts = data_points()
-            ax.plot(range(10), dpts, marker='o', color='orange')
-            graph.draw()
-            time.sleep(1)
-
-    def gui_handler():
-        change_state()
-        threading.Thread(target=plotter).start()
-
-    b = Button(root, text="Start/Stop", command=gui_handler, bg="red", fg="white")
-    b.pack()
-
-    root.mainloop()
-
-
-if __name__ == '__main__':
-    app()
